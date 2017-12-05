1e01c633c2ba576b65e786c445dc5a9b602755b4
CaptainSpaceCat <connor.settle@gmail.com>
2017-12-05 00:49:24 -0800

    Update ListNode.cpp

--- BucketList/ListNode.cpp
+++ BucketList/ListNode.cpp
@@ -1,11 +1,17 @@
 #include "Arduino.h"
 #include "ListNode.h"
 
+/* constructor
+ * this is a simple node object storing a double and a pointer to the next node
+ */
 ListNode::ListNode(double val = 0) {
     value = val;
     next = nullptr;
 }
 
+/* prints the bucket to Arduino's Serial monitor
+ * I would've made this return a string but C strings are disgusting abominiations
+ */
 void ListNode::toString() {
     Serial.print(value);
 }
